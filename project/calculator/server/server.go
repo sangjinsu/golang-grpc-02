bbc34b129102e9abc717b027f15a610d586213a6
jinsuSang <climbplant39@kakao.com>
2022-01-09 19:29:46 +0900

    add evans

--- project/calculator/server/server.go
+++ project/calculator/server/server.go
@@ -3,10 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+
 	"github.com/grpc-project02/project/calculator/calculatorpb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc/status"
+
 	"io"
 	"log"
 	"math"
@@ -120,6 +123,8 @@ func main() {
 
 	s := grpc.NewServer()
 	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+
+	reflection.Register(s)
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v\n", err)
